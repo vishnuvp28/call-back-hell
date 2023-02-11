71e60aa616cefae3b1816918ac129ad795ecb39b
vishnuvp28 <priyachandrasekarthik@gmail.com>
2023-02-12 00:15:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(()=>{
+    document.getElementById('callback').innerHTML=10;
+    setTimeout(()=>{
+        document.getElementById('callback').innerHTML=9;
+        setTimeout(()=>{
+            document.getElementById('callback').innerHTML=8;
+            setTimeout(()=>{
+                document.getElementById('callback').innerHTML=7;
+                setTimeout(()=>{
+                    document.getElementById('callback').innerHTML=6;
+                    setTimeout(()=>{
+                        document.getElementById('callback').innerHTML=5;
+                        setTimeout(()=>{
+                            document.getElementById('callback').innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById('callback').innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById('callback').innerHTML=2;
+                                    setTimeout(()=>{
+                                        document.getElementById('callback').innerHTML=1;
+                                        setTimeout(()=>{
+                                            document.getElementById('callback').innerHTML="Happy Independence Day";
+                                        },1000)
+                                     },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
